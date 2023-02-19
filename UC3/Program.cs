b54174d7dff7c496818b1ed30ef37203810baf45
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-19 22:03:02 +0530

    Add:comparsion

---
+++ UC3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace UC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of cooridinate x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate x3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate y3");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate x4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of cooridinate y4");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            double first = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double second = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            
+            Console.WriteLine("Length of Line1:" + first);
+            Console.WriteLine("Length of Line2:" + second);
+
+            double compare = first.CompareTo(second);
+
+            if (compare >= 0)
+            {
+                Console.WriteLine("First line is greater than second line");
+            }
+            else
+            {
+                Console.WriteLine("Second line is greater than first line");
+            }
+            Console.ReadLine();
+        }
+    }
+}
